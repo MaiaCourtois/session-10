46e0ecef6db18b1c8d1bf76fb807e89fdf857946
audio <audio@PST6916.local>
2017-11-17 16:45:49 +0100

    exos_var_graphs

---
+++ exo_var.js
@@ -0,0 +1,18 @@
+var etudiante = {
+  nom: "Che",
+  prenom: "Jenny"
+  age: 25,
+  pointure: 37,5,
+  cheveux: 'brun',
+  egocentrique: false,
+  super sympa: true
+},
+
+{nom: "Daire",
+prenom: "Constance"
+age: 22,
+pointure: 37,
+cheveux: 'brun'
+ennuyeuse: false
+positive: true
+}
